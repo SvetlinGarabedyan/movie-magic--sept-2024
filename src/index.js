758a3c8955ca028b32dde3d33df9b62e859c5a37
SvetlinGarabedyan <svetlingarabedyan@yahoo.com>
2024-09-29 15:22:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Just Works');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Just works!')
+})
+
+app.listen(1000, () => console.log('Server is listening on http://localhost:1000...'));
