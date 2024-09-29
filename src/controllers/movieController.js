d57135009ba494f233f597131b3c4cc4cf043073
SvetlinGarabedyan <svetlingarabedyan@yahoo.com>
2024-09-30 01:57:42 +0200

    Add details page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -13,7 +13,14 @@ router.post('/create', async(req, res) => {
     await movieService.create(movieData);
 
     
-    res.end();
+    res.redirect('/');
 })
+
+router.get('/:movieId/details',async (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = await movieService.getOne(movieId);
+
+    res.render('movies/details', {movie})
+});
 
 export default router;
